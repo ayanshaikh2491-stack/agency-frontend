a74a0141faf04465ec6871d1f5a564f7c2be3aa2
Ayan <ayanshaikh2491@gmail.com>
2026-08-06 20:52:07 +0530

    chore: remove scratch and nested junk files from repo

--- agency-frontend/check_indent.py
+++
@@ -1,4 +0,0 @@
-with open('src/app/admin/agents/sba/page.js', 'r', encoding='utf-8') as f:
-    lines = f.readlines()
-for i in range(1164, 1170):
-    print('Line {}: {}'.format(i+1, repr(lines[i][:80])))
--- check_line.py
+++
@@ -1,4 +0,0 @@
-with open('C:/Users/TAUSHEF/Downloads/int/agency-frontend/src/app/admin/agents/sba/page.js', 'r', encoding='utf-8') as f:
-    lines = f.readlines()
-for i in range(620, 625):
-    print(f'Line {i+1}: {repr(lines[i][:100])}')
